0bd815f3c575a18dfd2bd8f22b8d00e99dfa2b80
Niji M Joy <nijimjoy@gmail.com>
2025-11-07 13:32:28 -0800

    Fixed server.js to start backend server properly

--- server/src/server.js
+++ server/src/server.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config()
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
